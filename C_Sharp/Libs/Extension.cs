1fb9f206201fc9b8956341c9ed90014558bbce5b
horetha <horeth@deadlinesolutions.com>
2021-12-02 15:54:49 -0500

    initial commit

---
+++ C_Sharp/Libs/Extension.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SubArrayTesting
+{
+    /// <summary>
+    /// Extension class with Generic methods
+    /// </summary>
+    public static class Extension
+    {
+        /// <summary>
+        /// Get a sub array by using System.Copy
+        /// </summary>
+        /// <typeparam name="T">Generic array</typeparam>
+        /// <param name="array">The array to get a sub array from</param>
+        /// <param name="offset">The starting point (index) for the sub array</param>
+        /// <param name="length">The length of the sub array</param>
+        /// <returns>A sub array specified by the offset and length parameters</returns>
+        public static T[] SubArrayCopy<T>(this T[] array, int offset, int length)
+        {
+            ///NOTE: This seems to be the most efficient (also doesn't need additional libraries)
+            T[] result = new T[length];
+            Array.Copy(array, offset, result, 0, length);
+            return result;
+        }
+
+        /// <summary>
+        /// Get a sub array by using Enumerable.Skip and Enumerable.Take (from System.Linq)
+        /// </summary>
+        /// <typeparam name="T">Generic array</typeparam>
+        /// <param name="array">The array to get a sub array from</param>
+        /// <param name="offset">The starting point (index) for the sub array</param>
+        /// <param name="length">The length of the sub array</param>
+        /// <returns>A sub array specified by the offset and length parameters</returns>
+        public static T[] SubArraySkip<T>(this T[] array, int offset, int length)
+        {
+            return array.Skip(offset)
+                .Take(length)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Get a sub array by using ArraySegment
+        /// </summary>
+        /// <typeparam name="T">Generic array</typeparam>
+        /// <param name="array">The array to get a sub array from</param>
+        /// <param name="offset">The starting point (index) for the sub array</param>
+        /// <param name="length">The length of the sub array</param>
+        /// <returns>A sub array specified by the offset and length parameters</returns>
+        public static T[] SubArraySegment<T>(this T[] array, int offset, int length)
+        {
+            return new ArraySegment<T>(array, offset, length)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Get a sub array by using List.GetRange (from System.Collections.Generic)
+        /// </summary>
+        /// <typeparam name="T">Generic type</typeparam>
+        /// <param name="array">The array to get a sub array from</param>
+        /// <param name="offset">The starting point (index) for the sub array</param>
+        /// <param name="length">The length of the sub array</param>
+        /// <returns>A sub array specified by the offset and length parameters</returns>
+        public static T[] SubArrayGetRange<T>(this T[] array, int offset, int length)
+        {
+            ///NOTE: This seems to be the slowest
+            return new List<T>(array)
+                .GetRange(offset, length)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Reset an array to it's initialized values
+        /// </summary>
+        /// <typeparam name="T">Generic type</typeparam>
+        /// <param name="array">The array to re-initialize</param>
+        public static void ResetArray<T>(this T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                // default(T) will return the default value for whatever type T is
+                // For example, if T is an int, default(T) would return 0
+                array[i] = default(T);
+            }
+        }
+    }
+}
---
+++ C_Sharp/SubArrayTesting/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SubArrayTesting
+{
+    public class Program
+    {
+        public static long[] testArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 5, 6, 7, 7, 5, 7, 88, 45, 6, 345, 672, 5646, 234, 346
+                , 2342, 57457548, 879797, 35413, 42, 24, 124, 143453, 72368, 23834, 123123, 12312, 2523, 5654, 76, 1, 7, 4, 1, 123, 514, 7, 6
+                , 46, 146, 1245, 123, 424, 535646, 3453, 2342, 234 };
+        public static int offset;
+        public static int length = 0;
+
+        public static void Reset()
+        {
+            Extension.ResetArray(testArray);
+            offset = 5; // testArray[5] = 6
+            length = testArray.Length - offset;
+        }
+
+        public static void Main(string[] args)
+        {
+            BenchMark timeTester = new BenchMark();
+
+            Reset();
+            Console.WriteLine($"Starting tests with array of length: {testArray.Length}, offset of: {offset} and length of: {length}");
+
+            timeTester.Start();
+
+            // Array.Copy test
+            Reset();
+            testArray.SubArrayCopy(offset, length);
+            timeTester.Mark($"Array.Copy test");
+
+            // Enumerable.Skip and Enumerable.Take test
+            Reset();
+            testArray.SubArrayCopy(offset, length);
+            timeTester.Mark($"Enumerable.Skip and Enumerable.Take test");
+
+            // ArraySegment<T> Struct test
+            Reset();
+            testArray.SubArrayCopy(offset, length);
+            timeTester.Mark($"ArraySegment<T> Struct test");
+
+            // List.GetRange() test
+            Reset();
+            testArray.SubArrayCopy(offset, length);
+            timeTester.Mark($"List.GetRange() test");
+
+            timeTester.End();
+            timeTester.Report();
+        }
+    }
+}
