bb8548cf7e8c7e6d797b2679a4251087523680ff
horetha <horeth@deadlinesolutions.com>
2021-12-02 20:45:17 -0500

    Bubble sort algorithm

--- C_Sharp/Libs/Alg/Sorting.cs
+++ C_Sharp/Libs/Alg/Sorting.cs
@@ -18,25 +18,23 @@ namespace Alg
         /// 2) Remaining subarray which is unsorted.
         /// In every iteration of selection sort, the minimum element(considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.
         /// </summary>
-        /// <param name="array"></param>
-        /// <returns></returns>
+        /// <param name="array">The un-sorted array</param>
+        /// <returns>The array sorted</returns>
         public static int[] SelectionSort(this int[] array)
         {
             // edge case
             if (array.Length == 1)
                 return array;
 
-            // find the minimum of the array
-            int min = 0;
-
             // O(n^2) - 2 for loops
-            for(int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
+                // find the minimum of the array
                 // Step 1 assume min = current i
-                min = i;
-                
+                int min = i;
+
                 // Step 2 find the minimum element in the sub array
-                for(int j = i + 1; j < array.Length - 1; j++)
+                for (int j = i + 1; j < array.Length - 1; j++)
                 {
                     if(array[j] < array[min])
                     {
@@ -48,6 +46,33 @@ namespace Alg
                 int temp = array[i];
                 array[i] = array[min];
                 array[min] = temp;
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array">The un-sorted array</param>
+        /// <returns>The array sorted</returns>
+        public static int[] BubbleSort(this int[] array)
+        {
+            // edge case
+            if (array.Length == 1)
+                return array;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
             }
 
             return array;
--- C_Sharp/Libs/DriverConsoleApp/Program.cs
+++ C_Sharp/Libs/DriverConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace DriverConsoleApp
 {
     public class Program
     {
-        public static int[] testArray = { 12, 5, 1, 3, 2, 0, 1, 2, 3, 44, 55, 556, 6553, 1223, 234234, 2345235, 234251 };
+        public static int[] testArray = { 556, 5, 1, 3, 2, 0, 1, 2, 3, 44, 55, 12, 6553, 4, 234234, 2345235, 9 };
 
         public static void Main(string[] args)
         {
@@ -16,7 +16,7 @@ namespace DriverConsoleApp
             foreach (int i in testArray) Console.Write($"{i} ");
             Console.WriteLine();
 
-            testArray.SelectionSort();
+            testArray.BubbleSort();
             timeTester.Mark($"Array.Copy test");
 
             foreach (int i in testArray) Console.Write($"{i} ");
