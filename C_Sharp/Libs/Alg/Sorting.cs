908fb516610dae8b5a85bf0d4705ed3c734de7e0
horetha <horeth@deadlinesolutions.com>
2021-12-02 20:30:57 -0500

    Selection sort algorithm

--- C_Sharp/Libs/Alg/Sorting.cs
+++ C_Sharp/Libs/Alg/Sorting.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿///https://www.geeksforgeeks.org/fundamentals-of-algorithms/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,5 +10,47 @@ namespace Alg
     /// </summary>
     public static class Sorting
     {
+        /// <summary>
+        /// Selection sort:
+        /// The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. 
+        /// The algorithm maintains two subarrays in a given array.
+        /// 1) The subarray which is already sorted.
+        /// 2) Remaining subarray which is unsorted.
+        /// In every iteration of selection sort, the minimum element(considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int[] SelectionSort(this int[] array)
+        {
+            // edge case
+            if (array.Length == 1)
+                return array;
+
+            // find the minimum of the array
+            int min = 0;
+
+            // O(n^2) - 2 for loops
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                // Step 1 assume min = current i
+                min = i;
+                
+                // Step 2 find the minimum element in the sub array
+                for(int j = i + 1; j < array.Length - 1; j++)
+                {
+                    if(array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                // Step 3 swap min and i
+                int temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+
+            return array;
+        }
     }
 }
--- C_Sharp/Libs/DriverConsoleApp/Program.cs
+++ C_Sharp/Libs/DriverConsoleApp/Program.cs
@@ -1,54 +1,29 @@
 ﻿using Extension;
+using Alg;
 using System;
 
 namespace DriverConsoleApp
 {
     public class Program
     {
-        public static long[] testArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 5, 6, 7, 7, 5, 7, 88, 45, 6, 345, 672, 5646, 234, 346
-                , 2342, 57457548, 879797, 35413, 42, 24, 124, 143453, 72368, 23834, 123123, 12312, 2523, 5654, 76, 1, 7, 4, 1, 123, 514, 7, 6
-                , 46, 146, 1245, 123, 424, 535646, 3453, 2342, 234 };
-        public static int offset;
-        public static int length = 0;
-
-        public static void Reset()
-        {
-            Extension.Extension.ResetArray(testArray);
-            offset = 5; // testArray[5] = 6
-            length = testArray.Length - offset;
-        }
+        public static int[] testArray = { 12, 5, 1, 3, 2, 0, 1, 2, 3, 44, 55, 556, 6553, 1223, 234234, 2345235, 234251 };
 
         public static void Main(string[] args)
         {
             BenchMark.BenchMark timeTester = new BenchMark.BenchMark();
 
-            Reset();
-            Console.WriteLine($"Starting tests with array of length: {testArray.Length}, offset of: {offset} and length of: {length}");
-
             timeTester.Start();
+            foreach (int i in testArray) Console.Write($"{i} ");
+            Console.WriteLine();
 
-            // Array.Copy test
-            Reset();
-            testArray.SubArrayCopy(offset, length);
+            testArray.SelectionSort();
             timeTester.Mark($"Array.Copy test");
 
-            // Enumerable.Skip and Enumerable.Take test
-            Reset();
-            testArray.SubArrayCopy(offset, length);
-            timeTester.Mark($"Enumerable.Skip and Enumerable.Take test");
-
-            // ArraySegment<T> Struct test
-            Reset();
-            testArray.SubArrayCopy(offset, length);
-            timeTester.Mark($"ArraySegment<T> Struct test");
-
-            // List.GetRange() test
-            Reset();
-            testArray.SubArrayCopy(offset, length);
-            timeTester.Mark($"List.GetRange() test");
+            foreach (int i in testArray) Console.Write($"{i} ");
+            Console.WriteLine();
 
             timeTester.End();
-            timeTester.Report(true);
+            timeTester.Report(false);
         }
     }
 }
