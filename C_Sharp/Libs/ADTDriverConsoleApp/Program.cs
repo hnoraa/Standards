ee8ab2d0a7a63cd7bea2a4a9f6dbb8fb32b8ecaa
horetha <horeth@deadlinesolutions.com>
2021-12-08 16:05:02 -0500

    added a map like adt

--- C_Sharp/Libs/ADTDriverConsoleApp/Program.cs
+++ C_Sharp/Libs/ADTDriverConsoleApp/Program.cs
@@ -1,40 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ADTDriverConsoleApp
 {
     public class Heap
     {
-        private int _size;
-        private int[] _heap;
+        private int size;
+        private int[] heap;
 
         public Heap(int size)
         {
-            _size = 0;
-            _heap = new int[size];
+            size = 0;
+            heap = new int[size];
         }
 
         public void Print()
         {
-            Console.WriteLine($"{String.Join(",", _heap)}");
+            Console.WriteLine($"{String.Join(",", heap)}");
         }
 
         public int Size()
         {
-            return _size;
+            return size;
         }
 
         public bool IsEmpty()
         {
-            return _size == _heap.Length;
+            return size == heap.Length;
         }
 
         public void Insert(int item)
         {
             if(!IsEmpty())
             {
-                _heap[_size] = item;
+                heap[size] = item;
                 BubbleUp();
-                _size++;
+                size++;
             }
         }
 
@@ -42,15 +44,15 @@ namespace ADTDriverConsoleApp
         {
             if(!IsEmpty())
             {
-                _heap[0] = _heap[_size--];
+                heap[0] = heap[size--];
                 BubbleDown();
             }
         }
 
         private void BubbleUp()
         {
-            int index = _size - 1;
-            while(index > 0 && _heap[index] > _heap[Parent(index)])
+            int index = size - 1;
+            while(index > 0 && heap[index] > heap[Parent(index)])
             {
                 Swap(index, Parent(index));
                 index = Parent(index);
@@ -61,7 +63,7 @@ namespace ADTDriverConsoleApp
         {
             int index = 0;
 
-            while(index <= _size && !IsValidParent(index))
+            while(index <= size && !IsValidParent(index))
             {
                 int largerChildIndex = LargerChildIndex(index);
                 Swap(index, largerChildIndex);
@@ -71,9 +73,9 @@ namespace ADTDriverConsoleApp
 
         private void Swap(int index, int parent)
         {
-            int temp = _heap[index];
-            _heap[index] = _heap[parent];
-            _heap[parent] = temp;
+            int temp = heap[index];
+            heap[index] = heap[parent];
+            heap[parent] = temp;
         }
 
         private int Parent(int index)
@@ -92,10 +94,10 @@ namespace ADTDriverConsoleApp
         {
             if (!HasLeftChild(index)) return true;
 
-            bool isValid = _heap[index] >= LeftChild(index);
+            bool isValid = heap[index] >= LeftChild(index);
             if(HasRightChild(index))
             {
-                isValid &= _heap[index] >= RightChild(index);
+                isValid &= heap[index] >= RightChild(index);
             }
 
             return isValid;
@@ -113,22 +115,125 @@ namespace ADTDriverConsoleApp
 
         private int LeftChild(int index)
         {
-            return _heap[LeftChildIndex(index)];
+            return heap[LeftChildIndex(index)];
         }
 
         private int RightChild(int index)
         {
-            return _heap[RightChildIndex(index)];
+            return heap[RightChildIndex(index)];
         }
 
         private bool HasLeftChild(int index)
         {
-            return LeftChildIndex(index) <= _size;
+            return LeftChildIndex(index) <= size;
         }
 
         private bool HasRightChild(int index)
         {
-            return RightChildIndex(index) <= _size;
+            return RightChildIndex(index) <= size;
+        }
+    }
+
+
+    public class MapPoint
+    {
+        public string name;
+        public int id;
+        public List<int> connections;
+
+        public MapPoint(int mId, string mName)
+        {
+            connections = new List<int>();
+            id = mId;
+            name = mName;
+        }
+
+        public void AddConnection(int id)
+        {
+            if (!connections.Contains(id))
+                connections.Add(id);
+        }
+
+        public void RemoveConnection(int id)
+        {
+            if(connections.Contains(id))
+                connections.Remove(id);
+        }
+
+        public List<int> Connections()
+        {
+            return connections;
+        }
+
+        public void Describe()
+        {
+            Console.WriteLine($"Map Point: [{id}] {name} - ({connections.Count} connections)");
+        }
+    }
+
+    public class Map
+    {
+        public string name;
+        public int currentId;
+        public List<MapPoint> points;
+
+        public Map(string mName)
+        {
+            name = mName;
+            currentId = 0;
+            points = new List<MapPoint>();
+        }
+
+        public void AddPoint(string name)
+        {
+            points.Add(new MapPoint(currentId, name));
+            currentId++;
+        }
+
+        public void RemovePoint(int id)
+        {
+            MapPoint delPoint = points.FirstOrDefault(x => x.id == id);
+
+            if (delPoint != null)
+            {
+                points.Remove(delPoint);
+
+                foreach (MapPoint point in points)
+                {
+                    int? delId = point.connections.FirstOrDefault(x => x == id);
+                    if (delId != null)
+                    {
+                        point.RemoveConnection(id);
+                    }
+                }
+            }
+        }
+
+        public void ConnectPoints(int id1, int id2)
+        {
+            MapPoint p1 = points.FirstOrDefault(x => x.id == id1);
+            MapPoint p2 = points.FirstOrDefault(x => x.id == id2);
+
+            if(p1 != null && p2 != null)
+            {
+                p1.AddConnection(p2.id);
+                p2.AddConnection(p1.id);
+            }
+        }
+
+        public void Describe()
+        {
+            Console.WriteLine($"Map: {name}, ({points.Count} map points)");
+            foreach(MapPoint p in points)
+            {
+                p.Describe();
+
+                foreach(int id in p.connections)
+                {
+                    MapPoint mp = points.FirstOrDefault(x => x.id == id);
+                    Console.WriteLine($"{mp.name} is connected to {p.name}");
+                }
+            }
         }
     }
 
@@ -139,25 +244,44 @@ namespace ADTDriverConsoleApp
         public static void Main(string[] args)
         {
             int length = testArray.Length;
-            Heap h = new Heap(length);
+            //Heap h = new Heap(length);
 
             //Console.WriteLine($"Array\n\t{String.Join(",", testArray)}\n({length} elements)");
             //for(int i = 0; i < length; i++)
             //    h.Insert(testArray[i]);
 
-            h.Insert(1);
-            h.Insert(10);
-            h.Insert(59);
-            h.Insert(33);
-            h.Insert(15);
-            h.Insert(4);
-            h.Insert(20);
-            h.Insert(12);
-            h.Insert(270);
-            h.Insert(99);
-            h.Print();
-            h.Remove();
-            h.Print();
+            //h.Insert(1);
+            //h.Insert(10);
+            //h.Insert(59);
+            //h.Insert(33);
+            //h.Insert(15);
+            //h.Insert(4);
+            //h.Insert(20);
+            //h.Insert(12);
+            //h.Insert(270);
+            //h.Insert(99);
+            //h.Print();
+            //h.Remove();
+            //h.Print();
+
+            Map map = new Map("Test Map");
+            map.AddPoint("test point 1");
+            map.AddPoint("test point 2");
+            map.AddPoint("test point 3");
+            map.AddPoint("test point 4");
+            map.AddPoint("test point 5");
+
+            map.Describe();
+            
+            map.ConnectPoints(4, 3);
+            map.ConnectPoints(4, 2);
+            map.ConnectPoints(0, 1);
+            map.ConnectPoints(0, 1);
+            map.ConnectPoints(0, 2);
+            map.ConnectPoints(0, 3);
+            map.ConnectPoints(6, 9);
+
+            map.Describe();
         }
     }
 }
